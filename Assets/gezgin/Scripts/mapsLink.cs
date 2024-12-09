fc87ab33edd7856b2521ad3a8a5629e529a4500d
Eyüp Ensar Balık <ensar2001242096@gmail.com>
2024-12-09 22:08:12 +0300

    İstanbul tamamlandı

--- Assets/gezgin/Scripts/mapsLink.cs
+++ Assets/gezgin/Scripts/mapsLink.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mapsLink : MonoBehaviour
+public class mapsLink: MonoBehaviour
 {
-    public string mapsURL = "https://maps.app.goo.gl/uoTjZhgF7kzSfr9m9"; // Örneðin Sultanahmet'in koordinatlarý
+    public string mapsURL = ""; // Örneðin Sultanahmet'in koordinatlarý
 
     public void OpenMap()
     {
