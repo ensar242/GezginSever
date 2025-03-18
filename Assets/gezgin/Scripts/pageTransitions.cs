b7cf4e9a0fa020db0e1404dc40358f3132ac47a5
Eyüp Ensar Balık <ensar2001242096@gmail.com>
2025-03-18 03:50:20 +0300

    son dokunuş ve bitiş
    15 il eklendi uygulama genel manada bitti

--- Assets/gezgin/Scripts/pageTransitions.cs
+++ Assets/gezgin/Scripts/pageTransitions.cs
@@ -27,11 +27,68 @@ public class pageTransitions : MonoBehaviour
     {
         SceneManager.LoadScene("AFYONKARAHISAR");
     }
-
-    public void Ankara()
+    public void AGRI()
     {
-        SceneManager.LoadScene("Ankara");
+        SceneManager.LoadScene("AGRI");
     }
+    public void AMASYA()
+    {
+        SceneManager.LoadScene("AMASYA");
+    }
+    public void ANKARA()
+    {
+        SceneManager.LoadScene("ANKARA");
+    }
+    public void ANTALYA()
+    {
+        SceneManager.LoadScene("ANTALYA");
+    }
+    public void ARTVIN()
+    {
+        SceneManager.LoadScene("ARTVIN");
+    }
+    public void AYDIN()
+    {
+        SceneManager.LoadScene("AYDIN");
+    }
+    public void BALIKESIR()
+    {
+        SceneManager.LoadScene("BALIKESIR");
+    }
+    public void BILECIK()
+    {
+        SceneManager.LoadScene("BILECIK");
+    }
+    public void BINGOL()
+    {
+        SceneManager.LoadScene("BINGOL");
+    }
+    public void BITLIS()
+    {
+        SceneManager.LoadScene("BITLIS");
+    }
+    public void BOLU()
+    {
+        SceneManager.LoadScene("BOLU");
+    }
+    public void BURDUR()
+    {
+        SceneManager.LoadScene("BURDUR");
+    }
+    public void BURSA()
+    {
+        SceneManager.LoadScene("BURSA");
+    }
+    public void CANAKKALE()
+    {
+        SceneManager.LoadScene("CANAKKALE");
+    }
+    public void ELAZIG()
+    {
+        SceneManager.LoadScene("ELAZIG");
+    }
+
+
     public void Istanbul()
     {
         SceneManager.LoadScene("Istanbul");
